cef03ee34fcdb474265e75e378051f4823dd10f3
arismartono <arismartono@mjolnircode.org>
2026-03-09 20:14:59 +0700

    add base attr in vite congif

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  base: '/my-react-ts/',
   server: {
     port: 5173,       // paksa port 5173
     strictPort: true, // error jika port 5173 sudah dipakai
